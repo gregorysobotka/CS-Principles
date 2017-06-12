1eae2f1c807db299a4ee17a32481abc428cc4020
gregorysobotka <gregorysobotka@gmail.com>
2017-06-11 21:20:49 -0700

    adding palindrome

---
+++ JS/palindrome.js
@@ -0,0 +1,15 @@
+/*
+
+
+
+*/
+
+function isPalindrome(word){
+
+  for(var i=0; i<word.length; i++)
+    if(word.charAt(i)!==word.charAt(word.length-(i+1)))
+      return false
+
+  return true
+
+}
